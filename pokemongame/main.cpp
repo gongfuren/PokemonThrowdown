87bf8207bdef23018ba1b855ba0fa1fb028dadbd
Ian Cordero <ianpcordero@Ians-MacBook-Pro.local>
2013-08-06 16:29:55 -0700

    Initial Commit

---
+++ pokemongame/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  pokemongame
+//
+//  Created by Ian Cordero on 8/6/13.
+//  Copyright (c) 2013 Ian Cordero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
